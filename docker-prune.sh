c32d78a9ba86b3d68ed702e90d0221cd830cf277
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-02-05 09:28:06 +0100

    fix script

--- docker-prune.sh
+++ docker-prune.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
-# This script will remove all unused Docker objects:
+# This script stops all running Docker containers and then removes all unused Docker objects:
 # - Containers
 # - Networks
 # - Images
 # - Volumes
 #
-# Warning: This will remove all unused Docker resources. Be sure that you really want to do this.
+# Warning: This will stop running containers and remove unused Docker resources. Be sure that you really want to do this.
 
-read -p "WARNING: This will prune all unused Docker objects. Continue? (y/n): " response
+read -p "WARNING: This will stop all running containers and prune all unused Docker objects. Continue? (y/n): " response
 
 if [[ "$response" != "y" ]]; then
   echo "Operation cancelled."
   exit 0
 fi
 
+# Stop all currently running containers, if any.
+running_containers=$(docker ps -q)
+if [[ -n "$running_containers" ]]; then
+  echo "Stopping running containers..."
+  docker stop $running_containers
+else
+  echo "No running containers to stop."
+fi
+
 # Prune Docker system resources.
 # -a: Removes all unused images not just dangling ones.
 # --volumes: Also removes all unused volumes.
+echo "Pruning unused Docker resources..."
 docker system prune -a --volumes -f
 
 echo "Docker resources pruned."
