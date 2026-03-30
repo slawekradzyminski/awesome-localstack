f1c10d67eeca9b65e4373e3e58b259b47fe904ca
Slawek <slawomir.radzyminski@gmail.com>
2026-03-30 17:16:46 +0200

    Bump frontend image to 3.6.4

--- docker-run-all.sh
+++ docker-run-all.sh
@@ -58,7 +58,7 @@ docker run -d \
   --network my-private-ntwk \
   --name frontend \
   --expose 80 \
-  slawekradzyminski/frontend:3.6.3
+  slawekradzyminski/frontend:3.6.4
 
 echo "Starting Prometheus..."
 docker run -d \
